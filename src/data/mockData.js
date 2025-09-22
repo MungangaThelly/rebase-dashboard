b0f6ca68eb2d5724b660e0565e255aeca767e433
MungangaThelly <daddymulongo@yahoo.fr>
2025-09-22 17:22:17 +0200

    update delete and cleanup

--- src/data/mockData.js
+++
@@ -1,9 +0,0 @@
-const mockData = [
-  { time: '10:00', windSpeed: 5 },
-  { time: '11:00', windSpeed: 6 },
-  { time: '12:00', windSpeed: 8 },
-  { time: '13:00', windSpeed: 7 },
-  { time: '14:00', windSpeed: 9 },
-];
-
-export default mockData;
